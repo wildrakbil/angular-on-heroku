d63addf14c097c273fc7ad7705eaebaa27392352
William <wil8612@gmail.com>
2020-11-19 20:15:44 -0500

    error to deploy

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist"));
-app.all("*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist/index.html");
+app.use(express.static(__dirname + "/dist/angular-on-heroku"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/angular-on-heroku/index.html"));
 });
+
 app.listen(process.env.PORT || 8080);
